12cbc6ae42853be263ddafc37405191211140314
Anish9039 <anishthapa9039@gmail.com>
2024-08-26 15:14:27 +0545

    connect to frontend and ui changes

--- src/assets/component/Create.jsx
+++ src/assets/component/Create.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios  from 'axios'
+
 
 function Create (){
+ 
+  const [task , setTask] = useState([])
+
+  const Handleclick = () => {
+    axios .post('http://localhost:3001/add', {task:task})
+   .then(result => console.log(result))
+   .catch(err=> console.log(err))
+  } 
+
   return (
-    <div>
-     <input  type='text' name='' id='' />
-     <button type='button'> Add </button>
+
+
+    <div className='form' >
+     <input  type='text' name='' id='' style={{padding  : "10px" , width:"300px", borderBottom: "1px solid"}} onChange = {(e) => setTask(e.target.value)} />
+     <button type='button' style={{padding  : "10px" , cursor: "pointer", color:"white", backgroundColor:"black" }} onClick={Handleclick} > Add </button>
     </div>
   )
 }
--- src/assets/component/Home.jsx
+++ src/assets/component/Home.jsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react'
-import Create from './Create'
-
+import React, { useEffect, useState } from 'react';
+import Create from './Create';
+import axios from 'axios';
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 
 
   function Home(){
@@ -10,26 +12,54 @@ import Create from './Create'
     const styles = {
         center: {
           marginLeft: "auto",
-          marginRight: "auto"
+          marginRight: "auto",
+          alignItems: "center"
+ 
+          
         }
       }
+
+      useEffect (() => {
+        axios.get('http://localhost:3001/get')
+        .then(result => setTodo(result.data))
+        .catch( err => console.log(err))
+
+      }, [])
+
+
+      const handleEdit = (id) => {
+        axios.put('http://localhost:3001/update'+id)
+        .then(result => console.log(result))
+        .catch( err => console.log(err))
+
+      }
+
   return (
-    <div className={styles.center}  >
-         <h1>To do app</h1>
+    <div className='bar'>
+    <div className= {styles.center}  >
+         <h1>To-do</h1>
          <Create />
          {
 
             todo.length === 0
             ?
-            <div> <h2>No Record</h2> </div>
+             <div> <h2>No Record</h2> </div>
             :
             todo.map(todo => (
+                <div className='task'onClick={ () => handleEdit(todo._id) } style={{display:"flex",alignItems:"center", width:"345px", justifyContent: "space-between",backgroundColor: "black", color: "white", height:"35px",padding:"2px 5px 2px 5px" , marginTop:"3px"}} >
+                  <div className='checkbox'>
+                <CheckBoxOutlineBlankIcon className="check" />
+                <p>{todo.task}</p>
+                </div>
                 <div>
-                {todo}
+               <span> <DeleteOutlineIcon className="delete" /></span>
+                     </div>
+
                 </div>
             )) 
          }
       
+    </div>
     </div>
   )
 }
